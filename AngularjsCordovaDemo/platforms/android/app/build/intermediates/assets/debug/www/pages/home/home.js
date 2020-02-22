b1efeec409c63bbe1e786d561d9eaaca307e61be
Mihir Soni <mihirsoni855@gmail.com>
2020-02-22 10:48:28 +0530

    add angularcordova demo

---
+++ AngularjsCordovaDemo/platforms/android/app/build/intermediates/assets/debug/www/pages/home/home.js
@@ -0,0 +1,7 @@
+angular.module('app').component('home', {
+    templateUrl:  './pages/home/home.html',
+
+    controller: function() {
+        console.log("home component");
+    }
+})
---
+++ AngularjsCordovaDemo/www/pages/about/about.js
@@ -0,0 +1,7 @@
+angular.module('app').component('about', {
+    templateUrl:  './pages/about/about.html',
+
+    controller: function() {
+        console.log("about component");
+    }
+})
