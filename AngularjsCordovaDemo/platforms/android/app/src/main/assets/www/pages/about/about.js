3ffb5e1b676afc1a97a40f8af8939f723116e013
Mihir Soni <mihirsoni855@gmail.com>
2020-02-25 18:11:23 +0530

    init

--- AngularjsCordovaDemo/platforms/android/app/src/main/assets/www/pages/about/about.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').component('about', {
-    templateUrl:  './pages/about/about.html',
-
-    controller: function() {
-        console.log("about component");
-    }
-})
--- AngularjsCordovaDemo/platforms/android/app/src/main/assets/www/pages/home/home.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').component('home', {
-    templateUrl:  './pages/home/home.html',
-
-    controller: function() {
-        console.log("home component");
-    }
-})
